26c5e9a054ab4a330df13a7b0ed7c6487a78788f
thanhtrinh1532 <thanhtrinh15032000@gmail.com>
2025-07-23 13:38:34 +0700

    init code

---
+++ src/server.js
@@ -0,0 +1,14 @@
+// const express = require('express')
+import express from 'express'
+const app = express()
+
+const hostname = 'localhost'
+const port = 8017
+
+app.get('/', function (req, res) {
+  res.send('<h1> Hello world</h1>')  
+})
+
+app.listen(port, hostname, () => {
+    console.log(`Hello Trinh, server at http://${hostname}:${port}/`)
+})
